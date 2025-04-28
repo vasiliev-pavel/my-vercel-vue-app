03eed0b56c7050f3aeb600ce169b09d280b137dd
Your Name <vasilievpavel795@gmail.com>
2025-04-28 21:54:57 +0300

    add .gitignore and clean node_modules

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <h1>Hello from Vue + Vercel!</h1>
+    <button @click="callApi">Call API</button>
+    <p v-if="message">Message from API: {{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('')
+
+async function callApi() {
+  const res = await fetch('/api/hello')
+  const data = await res.json()
+  message.value = data.message
+}
+</script>
+
+<style scoped>
+.app {
+  padding: 2rem;
+  font-family: sans-serif;
+}
+</style>
