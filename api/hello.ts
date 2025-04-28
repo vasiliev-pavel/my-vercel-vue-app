deec4dff52b7152b676b58d4fe5d8f967421fb04
Your Name <vasilievpavel795@gmail.com>
2025-04-28 23:03:32 +0300

    fix serverless function export2

--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-module.exports = (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).json({ message: "Hello from Serverless Function!" });
-};
